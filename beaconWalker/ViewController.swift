3d777de9c072caa863d048a178cb025e487468f8
Peter Alt <peter.alt@philamuseum.org>
2016-10-03 18:41:53 -0400

    initial commit

--- beaconWalker/ViewController.swift
+++ beaconWalker/ViewController.swift
@@ -7,19 +7,163 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, BeaconSequenceDelegate {
+    
+    @IBOutlet weak var beaconTableView: UITableView!
+    
+    @IBOutlet weak var sequenceStatusView: UIView!
+    @IBOutlet weak var bluetoothStatusView: UIView!
+    @IBOutlet weak var accessStatusView: UIView!
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    @IBOutlet weak var activeSequenceView: UIView!
+    @IBOutlet weak var activeSequenceHeightConstraint: NSLayoutConstraint!
+    
+    
+    private var sequencePlaying = false
+    private var beaconSequencer : BeaconSequence!
+    
+    private let locationManager = CLLocationManager()
+    private let peripheralManager = CBPeripheralManager()
+    
+    private let defaultUUID = "f7826da6-4fa2-4e98-8024-bc5b71e0893e"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.bluetoothStatusView.layer.cornerRadius = self.bluetoothStatusView.frame.width / 2
+        self.accessStatusView.layer.cornerRadius = self.accessStatusView.frame.width / 2
+        self.activeSequenceHeightConstraint.constant = 0
+        
+        self.versionLabel.text = Helper.getVersion()
+        
+        let _ = Beacon.load(true, completion: { beacons in
+            print("beacons loaded: \(beacons.count)")
+            dispatch_async(dispatch_get_main_queue()) {
+                self.beaconTableView.reloadData()
+                self.beaconSequencer = BeaconSequence(beacons: beacons, delegate: self)
+                
+                self.checkAppRequirements()
+            }
+        })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func toggleSequence(sender: AnyObject) {
+        if CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if !Helper.Platform.isSimulator {
+        
+            if !(self.isBluetoothActive() && self.areLocationServicesAllowed()) {
+                print("something isn't right, we need permissions")
+                let alertController = UIAlertController(title: "Please verify settings", message: "This app requires Bluetooth to be enabled and access to Location Services (while in use only).\nPlease verify settings and try again.", preferredStyle: .Alert)
+                let cancelAction = UIAlertAction(title: "OK", style: .Cancel) { (action) in
+                }
+                alertController.addAction(cancelAction)
+                self.presentViewController(alertController, animated: true) {}
+                return
+            }
+        }
+        
+        self.checkAppRequirements()
+        
+        self.sequencePlaying = !self.sequencePlaying
+        if self.sequencePlaying {
+            
+            self.beaconSequencer.startSequence()
+            self.sequenceStatusView.backgroundColor = Helper.getColorGreen()
+            sender.setTitle("Stop Sequence", forState: .Normal)
+            
+            self.activeSequenceHeightConstraint.constant = 40
+            UIView.animateWithDuration(0.75, animations: {
+                self.view.layoutIfNeeded()
+            })
+            self.animateActiveSequenceView()
+            
+            
+        } else {
+            self.beaconSequencer.stopSequence()
+            self.sequenceStatusView.backgroundColor = Helper.getColorRed()
+            sender.setTitle("Start Sequence", forState: .Normal)
+            if let selectedRow = self.beaconTableView.indexPathForSelectedRow {
+                self.beaconTableView.deselectRowAtIndexPath(selectedRow, animated: true)
+            }
+            self.activeSequenceHeightConstraint.constant = 0
+            UIView.animateWithDuration(0.75, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
+    func beaconDidProgress(beacon: Beacon, index: Int) {
+        let indexPath = NSIndexPath(forItem: index, inSection: 0)
+        self.beaconTableView.selectRowAtIndexPath(indexPath, animated: true, scrollPosition: .Top)
+        self.beaconTableView.delegate?.tableView!(self.beaconTableView, didSelectRowAtIndexPath: indexPath)
+        self.advertiseBeacon(beacon)
+    }
+    
+    func advertiseBeacon(beacon: Beacon) {
+        
+        self.peripheralManager.stopAdvertising()
+        
+        let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: beacon.UUID)!, major: UInt16(beacon.major), minor: UInt16(beacon.minor), identifier: "beaconWalker")
+        
+        let pd = NSDictionary(dictionary: region.peripheralDataWithMeasuredPower(nil)) as! [String: AnyObject]
+        
+        self.peripheralManager.startAdvertising(pd)
+        
+        
+    }
 
+
+}
+
+extension ViewController {
+    
+    private func isBluetoothActive() -> Bool {
+        return self.peripheralManager.state == CBPeripheralManagerState.PoweredOn
+    }
+    
+    private func areLocationServicesAllowed() -> Bool {
+        return CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse
+    }
+    
+    private func animateActiveSequenceView() {
+        let colourAnim = CABasicAnimation(keyPath: "backgroundColor")
+        colourAnim.fromValue = self.activeSequenceView.backgroundColor
+        colourAnim.toValue = Helper.getColorYellow().CGColor
+        colourAnim.duration = 2.0
+        colourAnim.repeatCount = 5000
+        colourAnim.autoreverses = true
+        self.activeSequenceView.layer.addAnimation(colourAnim, forKey: "backgroundColorAnimation")
+    }
+    
+    private func checkAppRequirements() {
+        if self.isBluetoothActive() {
+            self.bluetoothStatusView.backgroundColor = Helper.getColorGreenDark()
+        } else {
+            self.bluetoothStatusView.backgroundColor = Helper.getColorRed()
+        }
+        
+        if self.areLocationServicesAllowed() {
+            self.accessStatusView.backgroundColor = Helper.getColorGreenDark()
+        } else {
+            self.accessStatusView.backgroundColor = Helper.getColorRed()
+        }
+    }
 
 }
 
